461c010d1d3a28b17261042edd65f61400686ae1
VGrolleau <virginie.grolleau+github@protonmail.com>
2022-10-31 20:15:47 +0100

    View styled components in React

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './pages/Home/index.jsx'
-import Survey from './pages/Survey/index.jsx'
-import Results from './pages/Results/index.jsx'
-import Freelances from './pages/Freelances/index.jsx'
-import Header from './components/Header/index.jsx'
-import Error from './components/Error/index.jsx'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
+import Header from './components/Header'
+import Error from './components/Error'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+    body {
+      margin: 0;
+    }
+`
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path="/">
--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,31 +1,62 @@
 import Card from '../../components/Card'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+
+const CardsContainer = styled.div`
+  display: grid;
+  gap: 24px;
+  grid-template-rows: 350px 350px;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  justify-items: center;
+`
+
+const PageTitle = styled.h1`
+  font-size: 30px;
+  color: black;
+  text-align: center;
+  padding-bottom: 30px;
+`
+
+const PageSubtitle = styled.h2`
+  font-size: 20px;
+  color: ${colors.secondary};
+  font-weight: 300;
+  text-align: center;
+  padding-bottom: 30px;
+`
 
 const freelanceProfiles = [
     {
         name: 'Jane Doe',
-        jobTitle: 'Devops'
+        jobTitle: 'Devops',
     },
     {
         name: 'John Doe',
-        jobTitle: 'Developpeur frontend'
+        jobTitle: 'Developpeur frontend',
     },
     {
         name: 'Jeanne Biche',
-        jobTitle: 'Développeuse Fullstack'
+        jobTitle: 'Développeuse Fullstack',
     },
 ]
 
 function Freelances() {
     return (
         <div>
-            <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
-            {freelanceProfiles.map((profile, index) => (
-                <Card
-                    key={`${profile.name}-${index}`}
-                    label={profile.jobTitle}
-                    title={profile.name}
-                />
-            ))}
+            <PageTitle>Trouvez votre prestataire</PageTitle>
+            <PageSubtitle>
+                Chez Shiny nous réunissons les meilleurs profils pour vous.
+            </PageSubtitle>
+            <CardsContainer>
+                {freelanceProfiles.map((profile, index) => (
+                    <Card
+                        key={`${profile.name}-${index}`}
+                        label={profile.jobTitle}
+                        title={profile.name}
+                    />
+                ))}
+            </CardsContainer>
         </div>
     )
 }
