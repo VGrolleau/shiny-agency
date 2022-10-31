f357c6b58639960ee514cd97c7c9515e26737185
VGrolleau <virginie.grolleau+github@protonmail.com>
2022-10-31 16:30:36 +0100

    View React Router

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home/index.jsx'
+import Survey from './pages/Survey/index.jsx'
+import Results from './pages/Results/index.jsx'
+import Freelances from './pages/Freelances/index.jsx'
+import Header from './components/Header/index.jsx'
+import Error from './components/Error/index.jsx'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/survey/:questionNumber">
+          <Survey />
+        </Route>
+        <Route path="/results">
+          <Results />
+        </Route>
+        <Route path="/freelances">
+          <Freelances />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
